d2ad507f742749cdeab6ac71576d907f713e88c8
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-17 16:38:33 +0200

    iteration 1 done

--- rooms-app/routes/index.routes.js
+++ rooms-app/routes/index.routes.js
@@ -1,8 +1,10 @@
-const { isLoggedIn } = require("../middleware/route-guard");
-
 const router = require("express").Router();
+const User = require("../models/User.model");
+const Room = require("../models/Room.model");
+const { isLoggedIn } = require("../middleware/route-guard");
+const { isLoggedOut } = require("../middleware/route-guard");
 
-/* GET home page */
+//GET home
 router.get("/", (req, res, next) => {
   const { currentUser } = req.session;
   res.render("index", { currentUser });
