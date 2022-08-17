4b8399932ba9c0e0cab03b2cc4a6e78048dae971
Adam Amatavivadhana <adam.amata@yahoo.com>
2022-08-17 18:55:27 +0200

    iteration 2 done without route-guard

--- rooms-app/routes/rooms.routes.js
+++ rooms-app/routes/rooms.routes.js
@@ -6,21 +6,59 @@ const { isLoggedOut } = require("../middleware/route-guard");
 
 //GET create
 router.get('/create', (req, res) => {
-    res.render('rooms/create');
+    User.find()
+        .then((allUsers) => {
+            res.render('rooms/create', { allUsers });
+        })
+        .catch(err => console.log(err)); 
 });
 
 //POST create
 router.post('/create', isLoggedIn, (req, res) => {
-    const { name, description, imageUrl } = req.body;
+    const { name, description, imageUrl, owner } = req.body;
 
     if (!name || !description) {
         res.render('rooms/create', { errorMessage: 'Please add a name and description'});
     }
     else {
-        Room.create({ name, description, imageUrl })
+        Room.create({ name, description, imageUrl, owner })
         .then(() => res.redirect('/'))
         .catch(err => console.log(err));
     }
-})
+});
+
+//GET edit
+router.get('/:id/edit', isLoggedIn, (req, res) => {
+    const { id } = req.params;
+    Room.findById(id)
+        .then((room) => {
+            res.render('rooms/edit', room);
+        })
+        .catch(err => console.log(err));
+});
+
+//POST edit
+router.post('/:id/edit', isLoggedIn, (req, res) => {
+    const { id } = req.params;
+    const { name, description, imageUrl} = req.body;
+
+    if (!name || !description){
+        res.render('rooms/edit', { errorMessage: 'Please add a name and description'});
+    }
+    else {
+        Room.findByIdAndUpdate(id, { name, description, imageUrl})
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err));
+    }
+});
+
+//POST delete
+router.post('/:id/delete', (req ,res) => {
+    const { id } = req.params;
+
+    Room.findByIdAndDelete(id)
+        .then(() => res.redirect('/'))
+        .catch(err => console.log(err));
+});
 
 module.exports = router;
